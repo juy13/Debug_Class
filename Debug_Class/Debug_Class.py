f96683ecca62a92dfa290323a440cbff1756a540
Julian <brilianti@outlook.com>
2019-07-09 16:03:40 +0300

    Сделан класс для отладки кода

--- Debug_Class/Debug_Class.py
+++ Debug_Class/Debug_Class.py
@@ -1,1 +1,29 @@
+import os
+import sys
+from accessify import protected
+
+class Debug:
+
+	def __init__(self, file_came, debug_came=True):
+		self.debug = debug_came
+		self.file = file_came
+
+	@protected
+	def log_in_file(self, log_info):
+		if self.debug == True:
+			with open(self.file, 'a') as handle:
+				handle.write(log_info + '\n')
+		else:
+			pass
+
+	@protected
+	def log_on_screen(self, log_info):
+		if self.debug == True:
+			print(log_info)
+		else:
+			pass
+
+	@protected
+	def log_lambda(self):
+		return lambda log_info: print(log_info) if self.debug == True else 0
 
